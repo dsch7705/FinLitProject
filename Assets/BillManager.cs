7ad5f0c6b59237a13a637ff771d442baeb84a3d2
Apriquat <dschooner23@eastwoodschools.org>
2022-04-01 14:20:34 -0400

    Bill Upgrading!!!

--- Assets/BillManager.cs
+++ Assets/BillManager.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class BillManager : MonoBehaviour
 {
+    Dictionary<int, int> bills = new Dictionary<int, int>();
+    Dictionary<int, int> upgradeCost = new Dictionary<int, int>();
+    public int currentBill = 1;
+    public int cost = 100;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PopulateBills();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void UpgradeBill()
+    {
+        if (MoneyManager.manager.money >= upgradeCost[currentBill + 1])
+        {
+            currentBill++;
+            Debug.Log("Bill upgraded to $" + bills[currentBill]);
+        }
+        else
+        {
+            Debug.Log("Not enough money!");
+        }
+    }
+
+    public void AddMoney()
+    {
+        MoneyManager.manager.AddMoney(bills[currentBill]);
+    }
+
+    void PopulateBills()
+    {
+        bills.Add(1, 1);
+        bills.Add(2, 5);
+        bills.Add(3, 10);
+        bills.Add(4, 20);
+        bills.Add(5, 50);
+        bills.Add(6, 100);
+
+        upgradeCost.Add(1, 0);
+        upgradeCost.Add(2, 100);
+        upgradeCost.Add(3, 500);
+        upgradeCost.Add(4, 1000);
+        upgradeCost.Add(5, 2000);
+        upgradeCost.Add(6, 50000);
     }
 }
